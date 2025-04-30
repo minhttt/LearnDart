d52853917125ce4fa330383086d21292aa57d3aa
minhttt <ascf5649@gmail.com>
2025-04-30 17:45:43 +0700

    update more about inheritance

--- inheritance.dart
+++ inheritance.dart
@@ -1,25 +1,36 @@
 main(List<String> args) {
   var user1 = User(20, "Minh", "minh123@gmail.com");
   user1.printInfo(); //--> Name: Minh, Age: 20 Email: minh123@gmail.com
+  user1.walk(); //--> Walking...
+  user1.isEmployee = true; //--> true
 }
 
 
-class Person {
+abstract class Person {
   int age = 0;
   String name = "";
   Person(this.age, this.name);
-  printInfo() {
-    print("Name: $name, Age: $age");
-  }
+  printInfo(); //abtract method || abtract function
 }
-class User extends Person {
+class User extends Person with WalkMan, Employee {
   String email = "";
   User(int age, String name, this.email) : super(age, name); 
   //super is used to call the constructor of the parent class
+  
+    @override
+    printInfo() {
+      print("Name: $name, Age: $age Email: $email");
+    }
+}
 
-  @override
-  printInfo() {
-    super.printInfo(); //--> Call the parent class method
-    print("Email: $email");
-  }
+mixin Employee {
+  bool isEmployee = false;
+}
+
+mixin WalkMan {
+    bool isWalk = false;
+    walk() {
+        isWalk = true;
+        print("Walking...");
+    }
 }
